3a557142a8c877c4dd668d0d304d0961409ec167
Joao Botelho <joao.botelho@unbabel.com>
2019-04-30 19:38:43 +0100

    Initial Commit

---
+++ CG-opengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CG-opengl
+//
+//  Created by João Botelho on 30/04/2019.
+//  Copyright © 2019 uc-joao-henrique. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
